4101a52011074fbb927548494b0d234098344e73
brohithkr <birthdayboard@4146c987a9a6>
2023-03-21 05:56:19 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from birthdayreminder import *
+from datetime import datetime
+from pytz import timezone
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def getbirthdaylist():
+    bdlist = getBDlist()
+    is_bd = (len(bdlist) != 0)
+    datetoday = datetime.now(timezone('Asia/Kolkata')).strftime("%d-%m-%Y %H:%M:%S")
+    return render_template("index.html",is_bd=is_bd,bdlist=bdlist,datetoday=datetoday)
+
+if __name__=="__main__":
+    app.run(True)
---
+++ birthdayreminder.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from pytz import timezone
+import sqlite3
+import os.path
+
+datetoday = datetime.now(timezone('Asia/Kolkata')).strftime("%d/%m/%y")
+# datetoday = "21/09/2005"
+
+def getBDlist():
+    global datetoday
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "students.db")
+    conn = sqlite3.connect(db_path)
+    cur = conn.cursor()
+    bdlist = []
+    sttable = cur.execute("select name,dob from students").fetchall()
+    for i in sttable:
+        if i[1][:5]==datetoday[:5]:
+            bdlist.append(i[0])
+    conn.close()
+    return bdlist
+
+if __name__=="__main__":
+    bdlist = getBDlist()
+    if len(bdlist)!=0:
+        print("Today is birthday of:")
+        for i in bdlist:
+            print(i)
+
